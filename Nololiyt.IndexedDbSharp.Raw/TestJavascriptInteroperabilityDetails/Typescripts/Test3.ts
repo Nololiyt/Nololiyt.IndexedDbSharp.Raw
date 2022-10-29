1a63316d91dd3b67f802fe268fad3d2bcee23118
yueyinqiu <lujiaqi2003@126.com>
2022-10-29 22:48:25 +0800

    add new property test

---
+++ Nololiyt.IndexedDbSharp.Raw/TestJavascriptInteroperabilityDetails/Typescripts/Test3.ts
@@ -0,0 +1,19 @@
+﻿class TestClass
+{
+    testProperty: number;
+
+    testMethod(): number
+    {
+        return this.testProperty;
+    }
+
+    constructor()
+    {
+        this.testProperty = Math.random();
+    }
+}
+
+export function newInstance()
+{
+    return new TestClass();
+}
